751e51cb443dd86644aa27dab1999195757ef7a9
John Arundel <john@bitfieldconsulting.com>
2023-02-15 17:27:11 +0000

    add count flag handling

---
+++ count/8/count.go
@@ -0,0 +1,113 @@
+package count
+
+import (
+	"bufio"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+)
+
+type counter struct {
+	files  []io.Reader
+	input  io.Reader
+	output io.Writer
+}
+
+type option func(*counter) error
+
+func WithInput(input io.Reader) option {
+	return func(c *counter) error {
+		if input == nil {
+			return errors.New("nil input reader")
+		}
+		c.input = input
+		return nil
+	}
+}
+
+func WithInputFromArgs(args []string) option {
+	return func(c *counter) error {
+		if len(args) < 1 {
+			return nil
+		}
+		c.files = make([]io.Reader, len(args))
+		for i, path := range args {
+			f, err := os.Open(path)
+			if err != nil {
+				return err
+			}
+			c.files[i] = f
+		}
+		c.input = io.MultiReader(c.files...)
+		return nil
+	}
+}
+
+func WithOutput(output io.Writer) option {
+	return func(c *counter) error {
+		if output == nil {
+			return errors.New("nil output writer")
+		}
+		c.output = output
+		return nil
+	}
+}
+
+func NewCounter(opts ...option) (counter, error) {
+	c := counter{
+		input:  os.Stdin,
+		output: os.Stdout,
+	}
+	for _, opt := range opts {
+		err := opt(&c)
+		if err != nil {
+			return counter{}, err
+		}
+	}
+	return c, nil
+}
+
+func (c counter) Lines() int {
+	lines := 0
+	input := bufio.NewScanner(c.input)
+	for input.Scan() {
+		lines++
+	}
+	for _, f := range c.files {
+		f.(io.Closer).Close()
+	}
+	return lines
+}
+
+func (c counter) Words() int {
+	words := 0
+	input := bufio.NewScanner(c.input)
+	input.Split(bufio.ScanWords)
+	for input.Scan() {
+		words++
+	}
+	for _, f := range c.files {
+		f.(io.Closer).Close()
+	}
+	return words
+}
+
+func Main() int {
+	lineMode := flag.Bool("lines", false, "Count lines, not words")
+	flag.Parse()
+	c, err := NewCounter(
+		WithInputFromArgs(flag.Args()),
+	)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 1
+	}
+	if *lineMode {
+		fmt.Println(c.Lines())
+	} else {
+		fmt.Println(c.Words())
+	}
+	return 0
+}
