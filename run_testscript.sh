c3f0af727b39395ef33ff606859ba305f6430c57
John Arundel <john@bitfieldconsulting.com>
2023-07-20 13:33:45 +0100

    test fixes

--- run_testscript.sh
+++ run_testscript.sh
@@ -9,5 +9,4 @@ LISTING=$1
 SCRIPT=${LISTING}-test.txtar
 # go install github.com/bitfield/txtar-c@latest
 txtar-c -quote -script ${LISTING}/test.txtar $LISTING >>$SCRIPT
-echo -n "${LISTING}... " && testscript $SCRIPT
-rm $SCRIPT
+echo -n "${LISTING}... " && testscript $SCRIPT && rm $SCRIPT
